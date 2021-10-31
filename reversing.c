0046149790bd2f8db7ebed2cbe9553d7d54ed3bf
Shivanii123 <93260978+Shivanii123@users.noreply.github.com>
2021-10-31 12:03:50 -0700

    created reversing.c
    to reverse a string

---
+++ reversing.c
@@ -0,0 +1,19 @@
+#include<stdio.h> 
+ int main() 
+ { 
+ char str[1000],rev[1000]; 
+ int i,j,count=0; 
+ scanf("%s",str); 
+ printf("\n String Before Reverse: %s",str); 
+   while(str[count]!='\0') 
+   { 
+    count++; 
+   } 
+   j = count-1; 
+    for(i=0;i<count;i++) 
+   { 
+     rev[i] = str[j]; 
+     j--; 
+   } 
+  printf("\nString After Reverse: %s", rev); 
+ }
